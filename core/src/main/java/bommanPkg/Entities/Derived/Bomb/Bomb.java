54f91dda587a4c4db8f31e92e1d6840c13a3ed6a
xor <benghia1st@gmail.com>
2021-12-11 22:03:13 +0700

    dat bom 0.6
    can cai: no dc dung cho, no chet nguoi

--- core/src/main/java/bommanPkg/Entities/Derived/Bomb/Bomb.java
+++ core/src/main/java/bommanPkg/Entities/Derived/Bomb/Bomb.java
@@ -41,33 +41,92 @@ public class Bomb extends Entity {
         // Center of the explosion
         gameMap.add(new Flame(getX(), getY(), getStage(), getGridPosX(), getGridPosY(), true));
 
+        int[] flameLengthByDirection = getFlameLengthByDirection(flameLength, gameMap);
+
         // Left
-        for (int i = 1; i <= flameLength; i++) {
-            if (validDirection(gameMap, Direction.LEFT)) {
-                gameMap.add(new Flame(getX() - i * gridSize, getY(), getStage(), getGridPosX() - i, getGridPosY(), false));
-            }
+        for (int i = 1; i <= flameLengthByDirection[1]; i++) {
+            gameMap.add(new Flame(getX() - i * gridSize, getY(), getStage(), getGridPosX() - i, getGridPosY(), false));
         }
 
         // Right
-        for (int i = 1; i <= flameLength; i++) {
-            if (validDirection(gameMap, Direction.RIGHT)) {
-                gameMap.add(new Flame(getX() + i * gridSize, getY(), getStage(), getGridPosX() + i, getGridPosY(), false));
-            }
+        for (int i = 1; i <= flameLengthByDirection[3]; i++) {
+            gameMap.add(new Flame(getX() + i * gridSize, getY(), getStage(), getGridPosX() + i, getGridPosY(), false));
         }
 
         // Up
-        for (int i = 1; i <= flameLength; i++) {
-            if (validDirection(gameMap, Direction.UP)) {
-                gameMap.add(new Flame(getX(), getY() + i * gridSize, getStage(), getGridPosX(), getGridPosY() + i, false));
-            }
+        for (int i = 1; i <= flameLengthByDirection[0]; i++) {
+            gameMap.add(new Flame(getX(), getY() + i * gridSize, getStage(), getGridPosX(), getGridPosY() + i, false));
         }
 
         // Down
+        for (int i = 1; i <= flameLengthByDirection[2]; i++) {
+            gameMap.add(new Flame(getX(), getY() - i * gridSize, getStage(), getGridPosX(), getGridPosY() - i, false));
+        }
+    }
+
+    private int[] getFlameLengthByDirection(int flameLength, GameMap gameMap) {
+        int[] flameDir = new int[4];
+        flameDir[0] = getFlameLengthUp(flameLength, gameMap);
+        flameDir[1] = getFlameLengthLeft(flameLength, gameMap);
+        flameDir[2] = getFlameLengthDown(flameLength, gameMap);
+        flameDir[3] = getFlameLengthRight(flameLength, gameMap);
+        return flameDir;
+    }
+
+    private int getFlameLengthRight(int flameLength, GameMap gameMap) {
+        int maxLength = 0;
+
         for (int i = 1; i <= flameLength; i++) {
-            if (validDirection(gameMap, Direction.DOWN)) {
-                gameMap.add(new Flame(getX(), getY() - i * gridSize, getStage(), getGridPosX(), getGridPosY() - i, false));
+            if (validDirection(gameMap, Direction.RIGHT, i)) {
+                maxLength = i;
+            } else {
+                break;
             }
         }
+
+        return maxLength;
+    }
+
+    private int getFlameLengthDown(int flameLength, GameMap gameMap) {
+        int maxLength = 0;
+
+        for (int i = 1; i <= flameLength; i++) {
+            if (validDirection(gameMap, Direction.DOWN, i)) {
+                maxLength = i;
+            } else {
+                break;
+            }
+        }
+
+        return maxLength;
+    }
+
+    private int getFlameLengthLeft(int flameLength, GameMap gameMap) {
+        int maxLength = 0;
+
+        for (int i = 1; i <= flameLength; i++) {
+            if (validDirection(gameMap, Direction.LEFT, i)) {
+                maxLength = i;
+            } else {
+                break;
+            }
+        }
+
+        return maxLength;
+    }
+
+    private int getFlameLengthUp(int flameLength, GameMap gameMap) {
+        int maxLength = 0;
+
+        for (int i = 1; i <= flameLength; i++) {
+            if (validDirection(gameMap, Direction.UP, i)) {
+                maxLength = i;
+            } else {
+                break;
+            }
+        }
+
+        return maxLength;
     }
 
 
@@ -76,22 +135,22 @@ public class Bomb extends Entity {
         return bombExploded;
     }
 
-    private boolean validDirection (GameMap gameMap, Direction dir) {
+    private boolean validDirection (GameMap gameMap, Direction dir, int pos) {
         int temp = 0;
         boolean valid = false;
 
         switch (dir) {
             case UP:
-                temp = gameMap.getGridMap()[getGridPosX()][getGridPosY() - 1];
+                temp = gameMap.getGridMap()[getGridPosX()][getGridPosY() - pos];
                 break;
             case DOWN:
-                temp = gameMap.getGridMap()[getGridPosX()][getGridPosY() + 1];
+                temp = gameMap.getGridMap()[getGridPosX()][getGridPosY() + pos];
                 break;
             case LEFT:
-                temp = gameMap.getGridMap()[getGridPosX() - 1][getGridPosY()];
+                temp = gameMap.getGridMap()[getGridPosX() - pos][getGridPosY()];
                 break;
             case RIGHT:
-                temp = gameMap.getGridMap()[getGridPosX() + 1][getGridPosY()];
+                temp = gameMap.getGridMap()[getGridPosX() + pos][getGridPosY()];
                 break;
         }
 
